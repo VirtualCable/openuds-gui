849796efd65c49d562e9fa857106a0e564724131
Adolfo Gómez García <dkmaster@dkmon.com>
2022-05-19 09:09:39 +0200

    enhanced safeHTML

--- src/app/gui/safe-html.pipe.ts
+++ src/app/gui/safe-html.pipe.ts
@@ -8,6 +8,14 @@ export class SafeHtmlPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
 
   transform(value: any, args?: any): any {
+    // Allow html, disallow scripts, onclick, etc.
+    value = value.replace(/<\s*script\s*/gi, '');
+    // Remove if exists any javascript event
+    // eslint-disable-next-line max-len
+    value = value.replace(/onclick|onmouseover|onmouseout|onmousemove|onmouseenter|onmouseleave|onmouseup|onmousedown|onkeyup|onkeydown|onkeypress|onkeydown|onkeypress|onkeyup|onchange|onfocus|onblur|onload|onunload|onabort|onerror|onresize|onscroll/gi, '');
+    // Remove if exists any javascript:
+    value = value.replace(/javascript\s*\:/gi, '');
+
     return this.sanitizer.bypassSecurityTrustHtml(value);
   }
 
