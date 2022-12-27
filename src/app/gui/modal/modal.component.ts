460d16d3b7dbc75333ed447d9c61a6c93314bf85
Adolfo Gómez García <dkmaster@dkmon.com>
2022-12-27 14:52:16 +0100

    upgraded from v14 to v15 angular

--- src/app/gui/modal/modal.component.ts
+++ src/app/gui/modal/modal.component.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-shadow */
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatLegacyDialogRef as MatDialogRef, MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA } from '@angular/material/legacy-dialog';
 import { interval, Subscription, Observable } from 'rxjs';
 
 declare const django: any;
