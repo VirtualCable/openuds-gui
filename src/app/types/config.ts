182651cfeef9a06a7acfa8ea45c613caceed48fb
Adolfo Gómez García <dkmaster@dkmon.com>
2025-11-14 00:01:19 +0100

    Add groups section to staff info and update user profile interface

--- src/app/types/config.ts
+++ src/app/types/config.ts
@@ -94,6 +94,7 @@ export interface UserProfile {
 }
 
 export interface Info {
+    readonly groups: string[];
     readonly networks: string[];
     readonly transports: string[];
     readonly ip: string;
